059696254898b76714e96bfbbd5292a041c8d3c4
lrcarrera <drewico@gmail.com>
2019-02-01 21:22:44 +0100

    create fake service to test

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private data: DataService) { }
+
 
   ngOnInit() {
+  }
+
+  firstClick() {
+    console.log('sup fool!');
+    this.h1Style = true;
   }
 
 }
