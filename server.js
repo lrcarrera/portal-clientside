2d88b30662f33060a321eb4e88a5b3547ece8000
lrcarrera <drewico@gmail.com>
2019-02-03 19:36:06 +0100

    fixing routes server.js

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/ng7-pre/index.html'));
 });
 
-var routes = require('./src/app/app-routing.module.ts'); //importing route
-routes(app); //register the route
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
