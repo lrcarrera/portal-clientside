dc59429978a40f5287672c69e3d06a3bf0d52a91
lrcarrera <drewico@gmail.com>
2019-02-03 19:30:28 +0100

    fixing routes

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/ng7-pre'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/ng7-pre/index.html'));
 });
+
+var routes = require('./src/app/app-routing.module.ts'); //importing route
+routes(app); //register the route
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
