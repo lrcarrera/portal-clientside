5e0e67cc3de1cae6d6445b7104599c15da03f495
lrcarrera <drewico@gmail.com>
2019-02-04 19:16:20 +0100

    testing post

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
+import { RootService } from '../root/root.service';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +10,10 @@ import { DataService } from '../data.service';
 export class HomeComponent implements OnInit {
 
   users: Object;
-  tasks: any;
+  customers: Object;
   h1Style: boolean;
 
-  constructor(private data: DataService) { }
+  constructor(private data: DataService, private customerService: RootService) { }
 
   ngOnInit() {
     this.data.getUsers().subscribe(data => {
@@ -20,18 +21,23 @@ export class HomeComponent implements OnInit {
         console.log(this.users);
       }
     );
-
+    this.customerService.postAPIData().subscribe(data => {
+        this.customers = data
+        console.log(this.customers);
+      }
+    );
+/*
     this.data.getTasks().subscribe(data => {
         this.tasks = data
         console.log(this.tasks);
       }
-    );
+    );*/
   }
 
   firstClick() {
     console.log('sup fool!');
     this.h1Style = true;
-    window.alert('tasks->'+this.tasks);
+   window.alert('tasks->'+this.customers);
   }
 
 }
