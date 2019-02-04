870297a31c2c29b21273983d44cc513b899dc31e
lrcarrera <drewico@gmail.com>
2019-02-04 16:29:11 +0100

    Connecting with backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng7-pre';
-}
+  restItems: any;
+    restItemsUrl = 'https://enigmatic-mountain-27495.herokuapp.com/hola';
+
+    constructor(private http: HttpClient) {}
+
+    ngOnInit() {
+      this.getRestItems();
+    }
+
+    // Read all REST Items
+    getRestItems(): void {
+      this.restItemsServiceGetRestItems()
+        .subscribe(
+          restItems => {
+            this.restItems = restItems;
+            console.log(this.restItems);
+          }
+        )
+    }
+
+    // Rest Items Service: Read all REST Items
+    restItemsServiceGetRestItems() {
+      return this.http
+        .get<any[]>(this.restItemsUrl)
+        .pipe(map(data => data));
+    }
+  }
